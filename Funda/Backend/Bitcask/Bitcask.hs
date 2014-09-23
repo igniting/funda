57c5c6868962622702e859e8dec52e89abf9086b
rabisg <guha.rabishankar@gmail.com>
2014-09-23 13:56:47 +0530

    Make Bitcask conform to typeclass

--- Funda/Backend/Bitcask/Bitcask.hs
+++ Funda/Backend/Bitcask/Bitcask.hs
@@ -1,16 +1,31 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies          #-}
 module Funda.Backend.Bitcask.Bitcask where
+
+import           Data.ByteString.Lazy.Char8
+import           Text.JSON                    as JSON
 
 import           Funda.Backend.Backend
 import           Funda.Backend.Bitcask.Delete as BitcaskDelete
 import           Funda.Backend.Bitcask.Get    as BitcaskGet
 import           Funda.Backend.Bitcask.Put    as BitcaskPut
 import           Funda.Backend.Bitcask.Types  as BitcaskTypes
+import           Funda.Backend.Serializable
 
 instance Backend Bitcask where
-  type Key   Bitcask = BitcaskTypes.Key
-  type Value Bitcask = BitcaskTypes.Value
-  type BackendMonad Bitcask = IO
-  get = BitcaskGet.get
-  put = BitcaskPut.put
-  delete = BitcaskDelete.delete
+  type Key     Bitcask = BitcaskTypes.Key
+  type Value   Bitcask = BitcaskTypes.Value
+  type QueryM  Bitcask = IO
+  type UpdateM Bitcask = IO
+  getRaw = BitcaskGet.get
+  putRaw = BitcaskPut.put
+  deleteRaw = BitcaskDelete.delete
+
+instance Serializable JSValue BitcaskTypes.Value where
+  encode = pack . JSON.encode
+  decode a = case JSON.decode $ unpack a of
+    JSON.Ok val -> Right val
+    JSON.Error err -> Left err
+
+instance SpecializedBackend Bitcask BitcaskTypes.Key JSON.JSValue
--- Funda/Backend/Bitcask/Get.hs
+++ Funda/Backend/Bitcask/Get.hs
@@ -1,9 +1,7 @@
 module Funda.Backend.Bitcask.Get where
 
-import           Data.ByteString
-
 import           Funda.Backend.Bitcask.Types
 
-get :: Bitcask -> Key -> IO Value
+get :: Bitcask -> Key -> IO (Maybe Value)
 get _ _ = do
-  return empty
+  return Nothing
