9377084ddf7dec179a93a11a2da7f53886990381
rabisg <guha.rabishankar@gmail.com>
2014-10-12 19:58:42 +0530

    Update typeclasses for RawBackend & Backend

--- Funda/Backend/Backend.hs
+++ Funda/Backend/Backend.hs
@@ -6,33 +6,46 @@ module Funda.Backend.Backend where
 
 import           Funda.Backend.Serializable
 
-class (Monad (QueryM b), Monad (UpdateM b)) =>  Backend b where
-  type Key     b
-  type Value   b
-  type QueryM  b :: * -> *
-  type UpdateM b :: * -> *
-  getRaw    :: b -> Key b -> QueryM b (Maybe (Value b))
-  putRaw    :: b -> Key b -> Value b -> UpdateM b ()
-  deleteRaw :: b -> Key b -> UpdateM b ()
+class (Monad (QueryM b), Monad (UpdateM b)) => RawBackend b where
+  data Key     b
+  data Value   b
+  data QueryM  b :: * -> *
+  data UpdateM b :: * -> *
+  getRaw    :: Key b -> QueryM b (Maybe (Value b))
+  putRaw    :: Key b -> Value b -> UpdateM b ()
+  deleteRaw :: Key b -> UpdateM b ()
+  runQuery  :: QueryM b a -> b -> a
+  runUpdate :: UpdateM b a -> b -> ()
 
-class (Backend b, Serializable key (Key b), Serializable value (Value b))
-      => SpecializedBackend b key value where
-  get :: b -> key -> QueryM b (Maybe value)
-  get backend k = getRaw backend (encode k) >>= (return . fromMaybe . fmap decode )
+class Database b k v where
+  query  :: (Backend b k v) => b -> k -> Maybe v
+  update :: (Backend b k v) => b -> k -> v -> ()
+  delete :: (Backend' b k)  => b -> k -> ()
+
+instance Database b k v where
+  query  backend key = runQuery (get key) backend
+  update backend key value = runUpdate (put key value) backend
+  delete backend key = runUpdate (del key) backend
+
+class (RawBackend b, Serializable key (Key b), Serializable value (Value b))
+      => Backend b key value where
+  get :: key -> QueryM b (Maybe value)
+  get k = getRaw (encode k) >>= (return . fromMaybe . fmap decode )
     where
       fromMaybe :: Maybe (Either String a) -> Maybe a
       fromMaybe Nothing            = Nothing
       fromMaybe (Just (Right a))   = Just a
       fromMaybe (Just (Left err))  = error $ "Could not decode. Data probably corrupted \n" ++ err
 
-  put :: b -> key -> value -> UpdateM b ()
-  put backend k v = putRaw backend (encode k) (encode v)
+  put :: key -> value -> UpdateM b ()
+  put k v = putRaw (encode k) (encode v)
 
-  delete :: b -> key -> UpdateM b ()
-  delete backend k = deleteRaw backend $ encode k
+class (RawBackend b, Serializable key (Key b)) => Backend' b key where
+  del :: key -> UpdateM b ()
+  del k = deleteRaw $ encode k
 
-instance (Backend backend, key ~ (Key backend), value ~ (Value backend))
-         => SpecializedBackend backend key value  where
-  get    = getRaw
-  put    = putRaw
-  delete = deleteRaw
+-- instance (Backend backend, key ~ (Key backend), value ~ (Value backend))
+--          => SpecializedBackend backend key value  where
+--   get    = getRaw
+--   put    = putRaw
+--   delete = deleteRaw
